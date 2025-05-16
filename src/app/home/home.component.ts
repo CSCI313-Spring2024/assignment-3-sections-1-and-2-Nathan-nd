fc36fe9136103a0ac6b0e8a4f316f3dc92704f01
Nathan-nd <nicho8ianethics@gmail.com>
2025-05-16 18:00:26 -0500

    Update the HomeComponent template to use @for

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,48 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housinglocation';
 
 @Component({
-
   selector: 'app-home',
+  standalone: true,
   imports: [CommonModule, HousingLocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
 
-housingLocation: HousingLocation = {
-  id: 9999,
-  name: 'Test Home',
-  city: 'Test city',
-  state: 'ST',
-  photo: `${this.baseUrl}/example-house.jpg`,
-  availableUnits: 99,
-  wifi: true,
-  laundry: false,
-};
+  housingLocationList: HousingLocation[] = [
+    {
+      id: 0,
+      name: 'Acme Fresh Start Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: `${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`,
+      availableUnits: 4,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 1,
+      name: 'A113 Transitional Housing',
+      city: 'Santa Monica',
+      state: 'CA',
+      photo: `${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg`,
+      availableUnits: 0,
+      wifi: false,
+      laundry: true,
+    },
+    {
+      id: 2,
+      name: 'Warm Beds Housing Support',
+      city: 'Juneau',
+      state: 'AK',
+      photo: `${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg`,
+      availableUnits: 1,
+      wifi: false,
+      laundry: false,
+    }
+  ];
 }
